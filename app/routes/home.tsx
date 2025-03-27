9eef2121da2c3d54c8e27e3a3999c18514df5dde
Ozuwaru <oswaldtg1@gmail.com>
2025-03-27 16:51:19 -0400

    schedule made

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,5 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Schedule from "~/Schedule/Schedule";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +9,5 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <Schedule/>;
 }
